e0c7444cc651b86e188e54d8da0f660a82481598
Dafydd Rusinow <Dafyddrusinow@gmail.com>
2024-09-29 02:01:11 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,22 +9,22 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector('.profile-image').addEventListener('click', function() {
         gsap.fromTo(".profile-image", 
             { 
-                scale: 1,  // Initial state (slightly larger)
+                scale: 1.2,  // Initial state (slightly larger)
                 x: 0, 
                 y: 0 
             }, 
             { 
-                scale: 1.2, // Make it grow slightly
-                duration: 0.3,
-                ease: "power1.out",
+                scale: 1, // Make it shrink slightly
+                duration: 0.5,
+                ease: "circ.out",
                 onComplete: function() {
                     // After growing, shrink it and move right and up
                     gsap.to(".profile-image", {
                         scale: 0.9,  // Shrinks smaller than original size
                         x: 180,       // Move to the right
                         y: -80,      // Move upwards
-                        duration: 0.4,
-                        ease: "bounce.out" // Adds a bounce effect
+                        duration: 0.35,
+                        ease: "circ.out" // Adds a bounce effect
                     });
                 }
             }
