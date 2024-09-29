71edf65f20cc41ccf0364dc2a902c358e486794e
Dafydd Rusinow <Dafyddrusinow@gmail.com>
2024-09-29 17:29:22 +0100

    a

--- js/main.js
+++ js/main.js
@@ -5,8 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
     // Set initial state for the profile-image box
     gsap.set(".profile-image", { scale: 1.2, x: 0, y: 0 });
 
+
+    const profileImage = document.querySelector('.profile-image');
+
+    function animateBox() {
+
     // Add click event listener to trigger animation
-    document.querySelector('.profile-image').addEventListener('click', function() {
         gsap.fromTo(".profile-image", 
             { 
                 scale: 1.2,  // Initial state (slightly larger)
@@ -17,7 +21,9 @@ document.addEventListener("DOMContentLoaded", function () {
                 scale: 1, // Make it shrink slightly
                 duration: 0.6,
                 ease: "circ.out",
+
                 onComplete: function() {
+                    
                     // After growing, shrink it and move right and up
                     gsap.to(".profile-image", {
                         scale: 0.9,  // Shrinks smaller than original size
@@ -29,5 +35,12 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             }
         );
-    });
+        profileImage.removeEventListener('click', animateBox);
+        
+    }
+    profileImage.addEventListener('click', animateBox);
+
+    function clickFunction() {
+        document.getElementById("firstClick").innerHTML = "";
+    }
 });
