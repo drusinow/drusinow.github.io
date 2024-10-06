e4af2f8d5c3139b6d44871d22c325525b4f45eb9
Dafydd Rusinow <Dafyddrusinow@gmail.com>
2024-10-07 00:59:48 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,6 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
         
         // Remove the click event listener after the first click
         profileImage.removeEventListener('click', animateBox);
+        document.getElementById("profilebox").innerHTML = "";
     }
 
     // Add click event listener
